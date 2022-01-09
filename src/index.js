0b62796cf5759a04b6daf4cab5486177f2d55c5d
ubahbojikob <zzzymzz1@gmail.com>
2022-01-09 14:36:13 +0300

    init: start reverse task

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = function reverse (n) {
   let a = n;
-  let b = a.toSrting();
+  let b = a.toString();
   let c = b.split('');
   let d = c.reverse();
   let e = d.join('');
   let f = Number(e);
-  return f;  
+  return f; 
 }
