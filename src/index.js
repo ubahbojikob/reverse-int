6d0abd008c78a6fe27d622ba5ee0cddcf8d461ee
ubahbojikob <zzzymzz1@gmail.com>
2022-01-09 14:58:14 +0300

    feat: update index file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 module.exports = function reverse (n) {
-  let a = n;
+  let a = Math.abs(n); 
   let b = a.toString();
   let c = b.split('');
   let d = c.reverse();
   let e = d.join('');
   let f = Number(e);
+  
   return f; 
-}
+};
+
