fd06338d2235f565eb575c6eac5d119e6c595e93
ubahbojikob <zzzymzz1@gmail.com>
2022-01-09 12:49:35 +0300

    init: add code to index

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function reverse (n) {
-  
+  let a = n;
+  let b = a.toSrting();
+  let c = b.split('');
+  let d = c.reverse();
+  let e = d.join('');
+  let f = Number(e);
+  return f;  
 }
